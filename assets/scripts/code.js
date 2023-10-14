44e7a38be1578a160fcfa56b4a21a67320c4fd1d
Lauri Tuovinen <lare.tuovinen@gmail.com>
2023-10-14 14:59:47 +0300

    added text to images

--- assets/scripts/code.js
+++ assets/scripts/code.js
@@ -38,17 +38,80 @@ function createClones(button){
     }
 }
 
-function loadDoc() {
+function loadSamurai() {
     const xhttp = new XMLHttpRequest();
     xhttp.onload = function() {
-      document.getElementById("demo").innerHTML = this.responseText;
+      document.getElementById("demoSamurai").innerHTML = this.responseText;
     }
-    xhttp.open("GET", "tekstia.txt");
+    xhttp.open("GET", "assets/texts/samurai.txt");
+    xhttp.send();
+}
+
+function loadCar() {
+    const xhttp = new XMLHttpRequest();
+    xhttp.onload = function() {
+      document.getElementById("demoCar").innerHTML = this.responseText;
+    }
+    xhttp.open("GET", "assets/texts/car.txt");
+    xhttp.send();
+}
+
+function loadCentaur() {
+    const xhttp = new XMLHttpRequest();
+    xhttp.onload = function() {
+      document.getElementById("demoCentaur").innerHTML = this.responseText;
+    }
+    xhttp.open("GET", "assets/texts/centaur.txt");
+    xhttp.send();
+}
+
+function loadDragon() {
+    const xhttp = new XMLHttpRequest();
+    xhttp.onload = function() {
+      document.getElementById("demoDragon").innerHTML = this.responseText;
+    }
+    xhttp.open("GET", "assets/texts/dragon.txt");
+    xhttp.send();
+}
+
+function loadFlyingship() {
+    const xhttp = new XMLHttpRequest();
+    xhttp.onload = function() {
+      document.getElementById("demoFlyingship").innerHTML = this.responseText;
+    }
+    xhttp.open("GET", "assets/texts/flyingship.txt");
+    xhttp.send();
+}
+
+function loadJet() {
+    const xhttp = new XMLHttpRequest();
+    xhttp.onload = function() {
+      document.getElementById("demoJet").innerHTML = this.responseText;
+    }
+    xhttp.open("GET", "assets/texts/jet.txt");
+    xhttp.send();
+}
+
+function loadMiniwalker() {
+    const xhttp = new XMLHttpRequest();
+    xhttp.onload = function() {
+      document.getElementById("demoMiniwalker").innerHTML = this.responseText;
+    }
+    xhttp.open("GET", "assets/texts/miniwalker.txt");
+    xhttp.send();
+}
+
+function loadWalker() {
+    const xhttp = new XMLHttpRequest();
+    xhttp.onload = function() {
+      document.getElementById("demoWalker").innerHTML = this.responseText;
+    }
+    xhttp.open("GET", "assets/texts/walker.txt");
     xhttp.send();
 }
 
 try {
-    loadDoc();
+    loadDCar();
 }
 catch(err) {
     document.getElementById("demo").innerHTML = err.message;
