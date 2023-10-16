c9ad6aa43d1606a1153ea49fd8a774d590295d86
Lauri Tuovinen <lare.tuovinen@gmail.com>
2023-10-16 21:29:39 +0300

    added to top button

--- assets/scripts/code.js
+++ assets/scripts/code.js
@@ -1,42 +1,13 @@
-let button = document.querySelector(".likeBtn");
-let icon = document.querySelector(".likeIcon");
-
-button.addEventListener("click", ()=>{
-    button.classList.toggle("active");
-    if(button.classList.contains("active")){
-        createClones(button);
-    }
-});
-
-function randomNum(min, max){
-    return Math.floor(Math.random()*(max- min +1)+min);
+function toLight() {
+    var element = document.body;
+    element.classList.toggle("bodyLight");
 }
 
-function negativePositive(){
-    return Math.random() <0.5 ? -1 : 1;
-}
+function redirect() {
+    alert('Redirecting');
+  }
 
-function createClones(button){
-    let numberOfClones = randomNum(2, 4);
-
-    for(let i=1; i <= numberOfClones; i++){
-        let clone = icon.cloneNode(true);
-        let size = randomNum(8, 20);
-        button.appendChild(clone);
-        clone.setAttribute("width", size);
-        clone.setAttribute("height", size);
-        clone.classList.add("clone");
-        clone.style.transform = 
-        "translate(" + 
-        negativePositive() * randomNum(15, 30) + "px," + 
-        negativePositive() * randomNum(15, 30) + "px)";
-        
-        let removeNode = setTimeout(() =>{
-            button.removeChild(clone);
-            clearTimeout(removeNode);
-        }, 800);
-    }
-}
+const toTop = () => window.scrollTo({top: 0, behavior: 'smooth'});
 
 function loadSamurai() {
     const xhttp = new XMLHttpRequest();
@@ -110,18 +81,10 @@ function loadWalker() {
     xhttp.send();
 }
 
+//error handling for trying different functions
 try {
     loadDCar();
 }
 catch(err) {
     document.getElementById("demo").innerHTML = err.message;
 }
-
-function toLight() {
-    var element = document.body;
-    element.classList.toggle("bodyLight");
-}
-
-function redirect() {
-    alert('Redirecting');
-  }
